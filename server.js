4c585a839ec837861af536076dba6bfc1f9beefe
Skip <skiphog@mail.ru>
2017-10-13 13:49:57 +0300

    initial commit

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+/**
+ * Главная страница
+ */
 app.get('/', function (req, res) {
   res.render('pages/index', {
     title: 'Добро пожаловать на AutoScout',
@@ -19,10 +22,16 @@ app.get('/', function (req, res) {
   });
 });
 
+/**
+ * Форма для добавления авто
+ */
 app.get('/auto', function (req, res) {
-  res.render('pages/auto');
+  res.render('pages/auto', {data: {}});
 });
 
+/**
+ * Добавить авто
+ */
 app.post('/auto', function (req, res) {
   var newId = 0;
   auto.forEach(function (a) {
@@ -35,6 +44,51 @@ app.post('/auto', function (req, res) {
     Name: req.body.name,
     Year: req.body.year,
     Price: req.body.price
+  });
+
+  res.redirect('/');
+});
+
+/**
+ * Форма редактирования авто
+ */
+app.get('/auto/:id/edit', function (req, res) {
+  var item = auto.find(function (obj) {
+    return obj.Id === +req.params.id;
+  });
+
+  if (item === undefined) {
+    return res.status(404).send('Нет машинки для редактирования');
+  }
+  res.render('pages/auto', {data: item});
+});
+
+/**
+ * Редактировать авто
+ */
+app.post('/auto/:id/edit', function (req, res) {
+
+  auto = auto.map(function (item) {
+    if (item.Id === +req.params.id) {
+      return {
+        Id: +req.params.id,
+        Name: req.body.name,
+        Year: req.body.year,
+        Price: req.body.price
+      };
+    }
+    return item;
+  });
+
+  res.redirect('/');
+});
+
+/**
+ * Удлить авто
+ */
+app.get('/auto/:id/delete', function (req, res) {
+  auto = auto.filter(function (obj) {
+    return obj.Id !== +req.params.id;
   });
 
   res.redirect('/');
